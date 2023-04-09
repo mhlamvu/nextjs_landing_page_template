af57f24c205a9fc93db448f698c6e71cb59544e9
mhlamvu <bafana.mhlamvu@gmail.com>
2023-04-09 22:05:04 +0200

    boilerplate stripped

---
+++ pages/_app.js
@@ -0,0 +1,16 @@
+// import '../styles/globals.css'
+import { ChakraProvider } from '@chakra-ui/react'
+import Navbar from '../components/Navbar'
+import Footer from '../components/Footer'
+
+function MyApp({ Component, pageProps }) {
+  return (
+    <ChakraProvider>
+      <Navbar />
+      <Component {...pageProps} />
+      <Footer />
+    </ChakraProvider>
+  )
+}
+
+export default MyApp
